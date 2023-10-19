b0c3ac5ff3f4cb3b4e9431dd79a2843edf4942c7
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 21:19:59 +0200

    update icon size calculated function.

--- src/components/icon/NativeIcon.tsx
+++ src/components/icon/NativeIcon.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 
 import {ColorValues} from '../../types/theme';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {scale} from '../../utils/responsive';
 import {useTheme} from '../../hooks';
+import {verticalScale} from '../../utils/responsive';
 
 export type IconName =
   | 'play'
@@ -25,7 +25,9 @@ interface Props {
 const NativeIcon: React.FC<Props> = ({name, color, size = 22}) => {
   const {theme} = useTheme();
 
-  return <Icon name={name} color={theme.colors[color]} size={scale(size)} />;
+  return (
+    <Icon name={name} color={theme.colors[color]} size={verticalScale(size)} />
+  );
 };
 
 export default NativeIcon;
