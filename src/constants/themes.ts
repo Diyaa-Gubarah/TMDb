596734bbae6678b4b05a61aae26bebbf0441a3ce
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 21:19:08 +0200

    create new color scheme for dark and light mode

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -9,19 +9,19 @@ export const DEFAULT_DARK_THEME_ID = "default-dark";
 
 
 export const DEFAULT_LIGHT_COLOR_THEME = {
-    textPrimary: '#040404', // Dark blue for primary text
-    textSecondary: '#6B778D', // Light blue-grey for secondary text
-    primary: '#E50914', // Netflix red for primary accents
-    background: '#FFFFFF', // Light background color
-    transparent: 'rgba(4, 4, 4, 0.1)' // Dark blue with transparency
+    textPrimary: '#040404',
+    textSecondary: '#6B778D',
+    primary: '#E50914',
+    background: '#FFFFFF',
+    transparent: 'rgba(4, 4, 4, 0.1)'
 };
 
 export const DEFAULT_DARK_COLOR_THEME = {
-    textPrimary: '#ffffff', // White text for dark mode
-    textSecondary: '#b3b3b3', // Light grey for secondary text
-    primary: '#E50914', // Netflix red for primary accents
-    background: '#000000', // Black background color
-    transparent: 'rgba(255, 255, 255, 0.1)' // White text with transparency
+    textPrimary: '#ffffff',
+    textSecondary: '#b3b3b3',
+    primary: '#E50914',
+    background: '#000000',
+    transparent: 'rgba(255, 255, 255, 0.1)'
 };
 
 
