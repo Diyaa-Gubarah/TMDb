b906605e112e6a888fc56080857b660124bcae19
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 20:08:05 +0200

    Update themes.ts

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -7,38 +7,23 @@ export const DEFAULT_DARK_THEME_ID = "default-dark";
 
 
 
-// export const DEFAULT_LIGHT_COLOR_THEME = {
-//     textPrimary: '#041C32',
-//     textSecondary: '#94a3b3',
-//     primary: '#ffcd33',
-//     background: '#F4FCFC',
-//     transparent: "#041C321A"
-// }
-
-// export const DEFAULT_DARK_COLOR_THEME = {
-//     textPrimary: '#ffffff',
-//     textSecondary: '#bdbdbd',
-//     primary: '#2196f3',
-//     background: '#212121',
-//     transparent: '#ffffff1A',
-// }
-
 
 export const DEFAULT_LIGHT_COLOR_THEME = {
-    textPrimary: '#041C32',
-    textSecondary: '#94a3b3',
-    primary: '#40E0D0',
-    background: '#F4FCFC',
-    transparent: '#94a3b31A'
+    textPrimary: '#040404', // Dark blue for primary text
+    textSecondary: '#6B778D', // Light blue-grey for secondary text
+    primary: '#E50914', // Netflix red for primary accents
+    background: '#FFFFFF', // Light background color
+    transparent: 'rgba(4, 4, 4, 0.1)' // Dark blue with transparency
 };
 
 export const DEFAULT_DARK_COLOR_THEME = {
-    textPrimary: '#fffbff',
-    textSecondary: '#d1e0e9',
-    primary: '#40E0D0',
-    background: '#181D31',
-    transparent: '#fffbff1A'
+    textPrimary: '#ffffff', // White text for dark mode
+    textSecondary: '#b3b3b3', // Light grey for secondary text
+    primary: '#E50914', // Netflix red for primary accents
+    background: '#000000', // Black background color
+    transparent: 'rgba(255, 255, 255, 0.1)' // White text with transparency
 };
+
 
 
 
