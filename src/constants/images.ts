2acfa1527c780cf6db47d3864ca14a7b8ba0cfce
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 20:01:46 +0200

    remove unnecessary components

--- src/constants/images.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    "empty": require("../assets/images/coin.gif")
-    // "empty": require("../assets/images/test.jpg")
-    // "empty": require("../assets/images/test.jpg")
-    // "empty": require("../assets/images/test.jpg")
-}
--- src/constants/variable.ts
+++
@@ -1,8 +0,0 @@
-// import Config from "react-native-config";
-
-// export const API_URL = Config.API_URL
-// export const GOOGLE_MAPS_API_KEY = Config.GOOGLE_MAPS_API_KEY
-
-
-
-export default {}
