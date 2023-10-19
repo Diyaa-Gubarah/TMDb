e48843088e2880c5f9d902eb12511c5538fcb862
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-20 00:48:56 +0200

    force app to always start from ltr no matter the device language.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+import App from './App';
+import {AppRegistry} from 'react-native';
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import {I18nManager} from 'react-native';
 import {name as appName} from './app.json';
+
+// Disable RTL layout and RTL support
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
+
 
 AppRegistry.registerComponent(appName, () => App);
