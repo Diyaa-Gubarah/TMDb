1a5b8fa19bbc5adfad471b25c60d432edddf2ca1
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 03:18:02 +0200

    create simple animation for movie list, and start building  detail screen.

--- src/screens/movie-detail/MovieDetail.tsx
+++ src/screens/movie-detail/MovieDetail.tsx
@@ -1,35 +1,153 @@
-import * as React from 'react';
+import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
+import {
+  NativeIcon,
+  NativeImage,
+  NativeText,
+  NativeView,
+  TouchIcon,
+} from '../../components';
 
-import {StyleSheet, Text, View} from 'react-native';
-
-import {AppNavigationProps} from '../../navigations/types';
-import {NativeText} from '../../components';
+import {GENRES} from '../../data/GENRES';
+import LinearGradient from 'react-native-linear-gradient';
+import React from 'react';
 import {useMovieStore} from '../../store/movie';
-import {withContainer} from '../../hoc';
+import {useTheme} from '../../hooks';
 
-type Props = AppNavigationProps<'MovieDetail'>;
+const {height, width} = Dimensions.get('window');
 
-const MovieDetail: React.FC<Props> = ({}) => {
+const MovieDetail = ({}) => {
+  const {theme} = useTheme();
   const selectedMovie = useMovieStore(state => state.selectedMovie);
-  return (
-    <View style={styles.container}>
-      <NativeText color="textPrimary">
-        {JSON.stringify(selectedMovie?.id)}
-      </NativeText>
-    </View>
+
+  const genres = React.useMemo(
+    () =>
+      GENRES.filter(genre => selectedMovie?.genre_ids.includes(genre.id)).map(
+        genre => genre.name,
+      ),
+    [selectedMovie?.id],
   );
+
+  console.log(selectedMovie?.genre_ids);
+
+  return selectedMovie ? (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{...styles.container, backgroundColor: theme.colors.background}}>
+      <View style={{width, height: height * 0.4}}>
+        <NativeImage
+          uri={`https://image.tmdb.org/t/p/original/${selectedMovie.backdrop_path}`}
+        />
+      </View>
+      <LinearGradient
+        colors={['transparent', theme.colors.background]}
+        style={{
+          width,
+          height: height * 0.4,
+          position: 'absolute',
+          top: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {!selectedMovie?.video && (
+          <View
+            style={{
+              padding: theme.spacing.md,
+              backgroundColor: `${theme.colors.textPrimary}70`,
+              borderRadius: theme.spacing.md,
+            }}>
+            <TouchIcon
+              name="play"
+              color="background"
+              size={28}
+              onPress={() => {}}
+            />
+          </View>
+        )}
+        <View
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            width,
+            padding: theme.spacing.lg,
+          }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                backgroundColor: theme.colors.textPrimary,
+                paddingHorizontal: theme.spacing.md,
+                borderRadius: theme.spacing.lg,
+              }}>
+              <NativeIcon name="star" color="background" size={12} />
+              <View style={{marginHorizontal: theme.spacing.xsm}} />
+              <NativeText size="sm" color="background">
+                {`${selectedMovie.vote_average}`}
+              </NativeText>
+            </View>
+            {React.Children.toArray(
+              genres.slice(0, 3).map(name => (
+                <View style={{marginLeft: theme.spacing.md}}>
+                  <NativeText size="sm" color="textPrimary" family="medium">
+                    {name}
+                  </NativeText>
+                </View>
+              )),
+            )}
+
+            <NativeText size="sm" color="textPrimary"></NativeText>
+          </View>
+        </View>
+      </LinearGradient>
+
+      <NativeView padding="lg">
+        <NativeText size="lg" family="bold" color="textPrimary">
+          {selectedMovie.title}
+        </NativeText>
+        <NativeText size="sm" color="textSecondary">
+          {`${selectedMovie.release_date} , ${selectedMovie.original_language}`}
+        </NativeText>
+        <NativeText
+          size="md"
+          color="textPrimary"
+          align="justify"
+          family="light">
+          {selectedMovie.overview}
+        </NativeText>
+      </NativeView>
+    </ScrollView>
+  ) : null;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  poster: {
+    width: '100%',
+    height: '50%',
+  },
+  detailsContainer: {
+    padding: 16,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+  },
+  releaseDate: {
+    fontSize: 16,
+    marginTop: 8,
+  },
+  overview: {
+    fontSize: 16,
+    marginTop: 16,
   },
 });
 
-export default withContainer(MovieDetail);
+export default MovieDetail;
--- src/screens/movie/MovieList.tsx
+++ src/screens/movie/MovieList.tsx
@@ -1,40 +1,93 @@
 import * as React from 'react';
 
+import {Animated, Dimensions, View} from 'react-native';
 import {
-  NativeImage,
-  NativeList,
+  NativeBackgroundImage,
   NativeText,
-  NativeTouch,
   NativeView,
+  TouchIcon,
 } from '../../components';
 
 import {AppNavigationProps} from '../../navigations/types';
+import BackgroundDrop from './BackgroundDrop';
 import {Movie} from '../../types/movie';
-import {View} from 'react-native';
 import {scale} from '../../utils/responsive';
 import {useMovieStore} from '../../store/movie';
 import useMoviesQuery from '../../query/useMoviesQuery';
-import {withContainer} from '../../hoc';
+import {useTheme} from '../../hooks';
+
+const screenWidth = Dimensions.get('window').width;
+const spacing = scale(4); // Adjust as needed
+const itemWidth = screenWidth * 0.75; // Adjust as needed
 
 type Props = AppNavigationProps<'MovieList'>;
 
 const MovieList: React.FC<Props> = ({navigation}) => {
   const setMovie = useMovieStore(state => state.setMovie);
   const {movies, isLoading, isError} = useMoviesQuery();
+  const {theme} = useTheme();
+
+  const scrollX = React.useRef(new Animated.Value(0));
+
+  const handleScroll = Animated.event(
+    [{nativeEvent: {contentOffset: {x: scrollX.current}}}],
+    {useNativeDriver: true},
+  );
+
+  const calculateTranslateY = (index: number) => {
+    return scrollX.current.interpolate({
+      inputRange: [
+        (index - 1) * itemWidth,
+        index * itemWidth,
+        (index + 1) * itemWidth,
+      ],
+      outputRange: [0, scale(-30), 0],
+    });
+  };
 
   const renderItem = React.useCallback(
-    (item: Movie, onPress: () => void) => (
-      <NativeView background="#">
-        <NativeTouch onPress={onPress} background="primary">
-          <NativeImage
-            uri={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
-            rounded={'sm'}
-            size={150}
-          />
-          <NativeText color="textPrimary">{item.title}</NativeText>
-        </NativeTouch>
-      </NativeView>
-    ),
+    (item: Movie, onPress: (item: Movie) => void, index: number) => {
+      return (
+        <View
+          style={{
+            width: itemWidth,
+            height: '60%',
+          }}>
+          <Animated.View
+            style={{
+              flex: 1,
+              transform: [{translateY: calculateTranslateY(index)}],
+              marginHorizontal: spacing,
+              borderRadius: theme.spacing.lg,
+              overflow: 'hidden',
+            }}>
+            <NativeBackgroundImage
+              uri={`https://image.tmdb.org/t/p/original/${item.poster_path}`}>
+              <View
+                style={{
+                  backgroundColor: `${theme.colors.background}60`,
+                  borderRadius: theme.spacing.lg,
+                  padding: theme.spacing.md,
+                  margin: theme.spacing.md,
+                }}>
+                <NativeText color="textPrimary" size="lg" family="bold">
+                  {item.title}
+                </NativeText>
+                <NativeText color="textPrimary" size="sm">
+                  {item.vote_average}
+                </NativeText>
+                <TouchIcon
+                  background="primary"
+                  name="play"
+                  color="textPrimary"
+                  onPress={() => onPress(item)}
+                />
+              </View>
+            </NativeBackgroundImage>
+          </Animated.View>
+        </View>
+      );
+    },
     [setMovie],
   );
 
@@ -59,16 +112,24 @@ const MovieList: React.FC<Props> = ({navigation}) => {
   }
 
   return (
-    <>
+    <NativeView>
       <NativeText color="textPrimary">Movie List</NativeText>
-      <NativeList
+      <BackgroundDrop data={movies?.results} scrollX={scrollX} />
+      <Animated.FlatList
+        horizontal
+        bounces={false}
+        onScroll={handleScroll}
+        contentContainerStyle={{alignItems: 'center'}}
         data={movies?.results}
         keyExtractor={keyExtractor}
-        renderItem={renderItem}
-        onPressItem={onPressItem}
+        renderItem={({item, index}) => renderItem(item, onPressItem, index)}
+        snapToAlignment={'start'}
+        snapToInterval={itemWidth} // item width + margin
+        scrollEventThrottle={16} // Adjust the value as needed
+        decelerationRate={0}
       />
-    </>
+    </NativeView>
   );
 };
 
-export default withContainer(MovieList);
+export default MovieList;
