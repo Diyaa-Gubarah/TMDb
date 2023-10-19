6de56a2d33df1ecd5b31ebb6733690037880fc89
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-19 20:07:41 +0200

    create utils function to trim long text.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,3 +12,11 @@ export const saveData = async (key: string, value: string) => {
 export const removeData = async (key: string) => {
   await SensitiveInfo.deleteItem(key, {});
 };
+
+
+export const truncateTitle = (title: string, maxLength: number = 10): string => {
+  if (title.length > maxLength) {
+    return `${title.substring(0, maxLength)}...`;
+  }
+  return title;
+}
