2a9d58324c3387c6743669794a11cc906cd836cd
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-10-20 22:01:39 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler';
 
 import * as React from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-import { AppNavigator } from './src/navigations/index';
-import { LightTheme } from './src/constants/themes';
-import { NativeStateBar } from './src/components';
-import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from './src/hooks/useTheme';
+import {AppNavigator} from './src/navigations/index';
+import {LightTheme} from './src/constants/themes';
+import {NativeStateBar} from './src/components';
+import {NavigationContainer} from '@react-navigation/native';
+import {ThemeProvider} from './src/hooks/useTheme';
 
 const queryClient = new QueryClient();
 
@@ -16,8 +16,8 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider initial={LightTheme}>
+        <NativeStateBar />
         <NavigationContainer>
-          <NativeStateBar />
           <AppNavigator />
         </NavigationContainer>
       </ThemeProvider>
